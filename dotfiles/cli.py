5951a644d3b28a0dcedc5777c32b8537bbddcc1b
Jon Bernard <jbernard@tuxion.com>
2014-12-05 22:01:33 -0500

    WIP

--- dotfiles/cli.py
+++ dotfiles/cli.py
@@ -10,6 +10,9 @@ from .core import Dotfiles as Repository
 from . import __version__
 
 
+import click
+
+
 CONFIG_FILE = '.dotfilesrc'
 
 
@@ -245,3 +248,40 @@ def main():
     repo = Repository(**repo_settings)
 
     dispatch(repo, cli_opts, args)
+
+
+# class Config(object):
+
+#     def __init__(self):
+#         self.verbose = False
+#         self.home_directory = '.'
+
+
+# pass_config = click.make_pass_decorator(Config, ensure=True)
+
+
+# @click.group()
+# @click.option('--verbose', is_flag=True)
+# @click.option('--home-directory', type=click.Path())
+# @pass_config
+# def main(config, verbose, home_directory):
+#     config.verbose = verbose
+#     if home_directory is not None:
+#         config.home_directory = home_directory
+
+
+# @main.command()
+# @click.option('--string', default='World',
+#               help='This is the thing that is greeted.')
+# @click.option('--repeat', default=1,
+#               help='How many times you should be greeted.')
+# @click.argument('out', type=click.File('w'), default='-',
+#                 required=False)
+# @pass_config
+# def say(config, string, repeat, out):
+#     """This script greets you."""
+#     if config.verbose:
+#         click.echo('We are in verbose mode')
+#     click.echo('Home directory is %s' % config.home_directory)
+#     for x in xrange(repeat):
+#         click.echo('Hello, %s!' % string, out)
